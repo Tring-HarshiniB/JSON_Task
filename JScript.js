2ccd1d2d74f93e02798f6e2cc4ecc6a0d87944e5
tringapps-harshini <102798974+tringapps-harshini@users.noreply.github.com>
2022-09-14 11:46:09 +0530

    Add files via upload

---
+++ JScript.js
@@ -0,0 +1,19 @@
+function myFunction(){
+    let tab = document.getElemetById("table2");
+    let row = tab.insertRow();
+    let cell_1 = row.insertCell();
+    let cell_2 = row.insertCell();
+    let cell_3 = row.insertCell();
+    let cell_4 = row.insertCell();
+    let cell_5 = row.insertCell();
+    let cell_6 = row.insertCell();
+    let cell_7 = row.insertCell();
+    
+    cell_1.innerHTML = "4";
+    cell_2.innerHTML = "hgdh";
+    cell_3.innerHTML = "r";
+    cell_4.innerHTML = "234";
+    cell_5.innerHTML = "654";
+    cell_6.innerHTML = "345";
+    cell_7.innerHTML = "345";
+}
